bffa25eb8ea21fe5265abcbfd4f02a1632ac11a8
walterbaya <walterbaya@yahoo.com>
2022-02-03 10:27:56 -0300

    se modifica el sizing de los titulos eliminando codigo basura

--- src/components/Biography.vue
+++ src/components/Biography.vue
@@ -3,29 +3,53 @@
     id="biography"
     class="container px-5 py-2 general-container background-seablue"
   >
-    <div class="row  rounded">
-      <div id="biography-container" class="col-12 d-flex justify-content-center align-items-center background-bluegrey color-greyblue mb-3">
+    <div class="row rounded">
+      <div
+        id="biography-container"
+        class="
+          col-12
+          d-flex
+          justify-content-center
+          align-items-center
+          background-bluegrey
+          color-greyblue
+          mb-3
+        "
+      >
         <i class="far fa-user-circle fa-3x mt-2 mx-2"></i>
-        <h1 class="p-2 mt-3 title color-greyblue" id="biography-title">Biografía</h1>
+        <h1 class="p-2 mt-3 title color-greyblue">
+          Biografía
+        </h1>
       </div>
     </div>
-    <div class="row mt-4 mt-md-5 mb-1 px-md-5">
+    <div class="row mt-4 mt-md-4 mb-1 px-md-5">
       <div class="col-12 col-md-4 p-0 m-0 d-flex justify-content-center">
         <img
           src="../assets/img/face.jpg"
-          class="w-100 img-fluid rounded border border-1"
+          class="w-100 img-fluid rounded border border-1 mt-4"
           alt="walter-baya-face"
         />
       </div>
-      <div class="col-12 col-md-8 px-md-3 px-md-5 mt-3 d-flex flex-column align-items-start">
-        <h1 class="color-greyblue px-md-2 pt-md-0 p-0 py-2">Walter Baya</h1>
-        <h2 class="text-black px-md-3 my-2 p-0 py-2 text-decoration-underline">Desarrollador de Software</h2>
-        <p class="text-black px-md-3 px-0 py-2 mt-2">
-          <strong>Apasionado por la tecnología y la computación</strong>, me motiva seguir
-          aprendiendo cosas nuevas y realizar proyectos poniendo a prueba dichos
-          aprendizajes.
+      <div
+        class="
+          col-12 col-md-8
+          px-md-3 px-md-5
+          mt-4
+          d-flex
+          flex-column
+          align-items-start
+        "
+      >
+        <h1 class="color-greyblue px-md-3 pt-md-0 p-0 py-1">Walter Baya</h1>
+        <h4 class="text-black px-md-4  pb-3 mt-md-0 text-decoration-underline">
+          Desarrollador de Software
+        </h4>
+        <p class="text-black px-md-4 px-1 mt-2">
+          <strong>Apasionado por la tecnología y la computación</strong>, me
+          motiva seguir aprendiendo cosas nuevas y realizar proyectos poniendo a
+          prueba dichos aprendizajes.
         </p>
-        <ul class="list-unstyled py-2">
+        <ul class="list-unstyled px-2">
           <li>
             <a href="" class="text-decoration-none text-dark py-3"
               ><i class="fas fa-envelope"></i
@@ -43,13 +67,26 @@
     </div>
     <div class="row px-md-5">
       <hr />
-      <div class="col-12 px-3">
-        <h1 class=" p-md-2 p-0 py-md-1 color-greyblue py-2">Estudios Universitarios</h1>
+      <div class="col-12">
+        <h1 class="p-0 py-md-1 color-greyblue py-2">
+          Estudios Universitarios
+        </h1>
         <ul class="list-unstyled">
-          <h2 class="px-md-1 p-0  py-1 mx-md-3  mx-1 py-md-1 text-dark" id="university">Universidad de Buenos Aires</h2>
+          <h4
+            class="
+              px-md-0
+              mx-md-1
+              py-md-1
+              px-1
+              text-dark text-decoration-underline
+            "
+            id="university"
+          >
+            Universidad de Buenos Aires
+          </h4>
           <li>
-            <p class="px-md-1 p-0  mx-md-3  mx-1 py-md-1 text-dark" id="degree">
-              Licenciatura en Ciencias de la Computación
+            <p class="px-md-0 px-2 mx-md-2 text-dark" id="degree">
+              Licenciatura en Ciencias de la Computación 
             </p>
           </li>
         </ul>

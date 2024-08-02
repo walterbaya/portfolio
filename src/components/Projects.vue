e424a118dd22d5f9ee1575e037606a93cdad3350
walterbaya <56239454+walterbaya@users.noreply.github.com>
2024-08-02 18:53:42 -0300

    Se reestablecen cambios

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -13,7 +13,7 @@
     </div>
     <div class="row px-3 pt-5">
       <div
-        class="col-12 col-xl-6"
+        class="col-12 col-md-6 col-xl-4"
         v-for="project in projects"
         v-bind:key="project.project"
       >
